a5b4eee7503dcc7b9eb81edae1397fe4e5c76d11
Fin Mavis <fin.mavis@yahoo.com>
2018-12-14 12:23:04 +0700

    Updates

---
+++ precache-manifest.0a97128074dc26e25385c17756d33888.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "377b31ea3711a9d6ba50",
+    "url": "/contact-list/static/js/runtime~main.377b31ea.js"
+  },
+  {
+    "revision": "4594eb0dca50661af87d",
+    "url": "/contact-list/static/js/main.4594eb0d.chunk.js"
+  },
+  {
+    "revision": "d9088fdd81b6f883c918",
+    "url": "/contact-list/static/js/1.d9088fdd.chunk.js"
+  },
+  {
+    "revision": "62c6b60e70d3c4bafa141f1a43ab59a4",
+    "url": "/contact-list/index.html"
+  }
+];
